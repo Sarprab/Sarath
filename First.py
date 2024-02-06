a4ab71d4e69262fdf2f0a9eaebce760f63c39741
Sarath <sarathprabhakaran@gmail.com>
2024-02-06 09:44:32 -0700

    Code change

--- First.py
+++ First.py
@@ -6,7 +6,7 @@ import random
 
 def get_choices():
     player_choice = input("Enter a choice rock, paper, scissors: ")
-    options = ["Rock1", "Paper", "Scissors"]
+    options = ["Rock", "Paper", "Scissors"]
     computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
     return choices
